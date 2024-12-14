a31e1012270babbba71a5a7acc95988619f2aff3
captainmehmett <mmhmtkptn60@gmail.com>
2024-12-14 23:50:39 +0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,22 +1,161 @@
 const questions = [
     {
-        question: "Hangisi bir kırık şüphesinde yapılması gereken ilk yardımlardan biridir?",
+        question: "Aşağıdakilerden hangisi elektrik devresindeki sigortanın görevidir?",
         options: [
-            "Yaralıyı hareket ettirmek",
-            "Kırık bölgeyi sabitlemek",
-            "Kanamayı durdurmadan bölgeyi sarmak",
-            "Hiçbir şey yapmadan sağlık ekiplerini beklemek"
+            "Aküyü şarj etmek",
+            "Bujilere giden akımı yükseltmek",
+            "Kısa devre olduğunda sistemi korumak",
+            "Endüksiyon bobinine giden akımı yükseltmek"
+        ],
+        correctAnswer: 2
+    },
+    {
+        question: "Aşağıdakilerden hangisi, motordaki basınçlı yanmış gazların çıkarken oluşturacağı sesi azaltır?",
+        options: [
+            "Emme susturucusu",
+            "Egzoz susturucusu",
+            "Emme manifoldu",
+            "Egzoz supabı"
+        ],
+        correctAnswer: 1
+    },
+    {
+        question: "Araçlarda hangisinin bulunması, olası bir kaza anında ölüm ve yaralanma riskini azaltır?",
+        options: [
+            "Alarm",
+            "Hava yastığı",
+            "Merkezi kilit",
+            "Hız sabitleyici"
+        ],
+        correctAnswer: 1
+    },
+    {
+        question: `I. Ambulans ekiplerince müdahaleler yapılması
+            II. Olay yerinde, ilk yardımcı tarafından temel yaşam desteği yapılması
+        III. Hastane acil servislerinde müdahale yapılması
+        IV. Sağlık kuruluşuna haber verilmesi
+
+        Hayat kurtarma zinciri halkalarının sıralaması hangi seçenekte doğru olarak verilmiştir?`,
+        options: [
+            "II - IV - I - III",
+            "IV - II - I - III",
+            "I - II - III - IV",
+            "II - III - IV - I"
+        ],
+        correctAnswer: 1 
+    },
+    {
+        question: " Aşağıdaki davranışlardan hangisi trafik adabına sahip bir sürücü için uygundur?",
+        options: [
+            "Trafik polisi yoksa kırmızı ışıkta geçmek",
+            "Sürekli şerit değiştirerek araç kullanmak",
+            "Denetim korkusu olmadan emniyet kemerini takmak",
+            "Elektronik denetleme sisteminin olmadığı yerlerde hız limitlerini aşmak"
+        ],
+        correctAnswer: 2
+    },
+
+    {
+        question: "Şekildeki trafik işaretini gören sürücünün aşağıdakilerden hangisini yapması yanlıştır?",
+        image: "images/yaya.png",
+        options: [
+            "Yolu kontrol etmesi",
+            "Öndeki aracı geçmesi",
+            "Aracının hızını azaltması",
+            " Geçiş hakkını yayalara vermesi"
+        ],
+        correctAnswer: 1
+    },
+
+    {
+        question: `• Görev hâlinde iken geçiş üstünlüğü hakkına sahiptir.
+                   • Görev hâli dışında geçiş üstünlüğü işaretini ve hakkını kullanması yasaktır.
+                   • Hizmetin yerine getirilmesini sağlamak amacına uygun olması şartıyla,
+                   Kara Yolları Trafik Kanunu ve bu Yönetmelikte gösterilen trafik kısıtlamalarına ve yasaklarına bağlı değildir.
+
+        Buna göre aşağıdakilerden hangisi sürülmelerine ilişkin esas ve kuralları verilen araçlardandır??`,
+        options: [
+            "Kamu hizmeti taşıtı",
+            "Okul taşıtı",
+            "İtfaiye",
+            "Çekici"
+        ],
+        correctAnswer: 2
+    },
+    {
+        question: "Şekildeki araçlardan hangileri beklemelidir?",
+        image: "images/arac.jpg",
+        options: [
+            "Yalnız 1",
+            "Yalnız 2",
+            "1 ve 3",
+            "2 ve 4"
         ],
         correctAnswer: 3
     },
-    {
-        question: "Aşağıdaki durumlardan hangisi kalp krizi belirtisidir?",
+
+     {
+        question: "Belgelerinde aksine bir hüküm yoksa şehirler arası bölünmüş kara yolunda tehlikeli madde taşıyan araçların azami hızı saatte kaç kilometredir?",
         options: [
-            "Ani baş dönmesi",
-            "Kol ve göğüs bölgesinde ağrı",
-            "Ayak bileklerinde şişlik",
-            "Karında gaz sancısı"
+            "40",
+            "50",
+            "60",
+            "70"
         ],
         correctAnswer: 2
-    }
+    },
+
+    {
+        question: "Şekildeki araç güç aktarma organlarının adları hangi seçenekte doğru olarak verilmiştir?",
+        image: "images/motor.png",
+        options: [
+            "I. Şaft - II. Diferansiyel - III. Aks",
+            "I. Şaft - II. Aks - III. Diferansiyel",
+            "I. Aks - II. Diferansiyel - III. Şaft",
+            "I. Diferansiyel - II. Aks - III. Şaft"
+        ],
+        correctAnswer: 0
+    },
+
+    {
+        question: "Aşağıdakilerden hangisi trafiğin düzenlenmesinde en yüksek önceliğe sahiptir?",
+        options: [
+            "Trafik ışıkları",
+            "Trafik görevlisi",
+            "Yer işaretleri",
+            "Trafik levhaları"
+        ],
+        correctAnswer: 1
+    },
+
+    {
+        question: "Kazazedeyi araçtan çıkarırken aşağıdakilerden hangisine dikkat edilmelidir?",
+        options: [
+            "Kollarının baş hizasında durmasına",
+            " Baş tarafından çekilerek çıkarılmasına",
+            "Ayak tarafından çekilerek çıkarılmasına",
+            "Baş-boyun-gövde hizasının bozulmamasına"
+        ],
+        correctAnswer: 3
+    },
+
+    {
+        question: "Şekildeki trafik işaretini gören sürücü aşağıdakilerden hangisini yapmalıdır?",
+        image: "images/sag.png",
+        options: [
+            " Takip mesafesini azaltmalıdır",
+            "Sağ dönüş lambasını yakmalıdır",
+            "Yolun solundan gitmelidir",
+            "Yavaşlayıp, varsa geçme yasağına uymalıdır"
+        ],
+        correctAnswer: 3
+    },
+
+
+   
+
+
+    
+    
+   
 ];
